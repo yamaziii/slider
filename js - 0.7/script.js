d2940f5325d05f017200b4e27f940f45afd4740e
yamaziii <106329302+yamaziii@users.noreply.github.com>
2022-06-28 18:38:21 +0600

    Add files via upload

---
+++ js - 0.7/script.js
@@ -0,0 +1,12 @@
+let swiper = new Swiper('.mySwiper', {
+	cssMode: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	mousewheel: true,
+	keyboard: true,
+})
